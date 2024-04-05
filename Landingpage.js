8ebbc7d6cc922716c1067c97eec93a98052e675b
Nhung141004 <161905582+Nhung141004@users.noreply.github.com>
2024-04-05 22:23:00 +0700

    Add files via upload

---
+++ Landingpage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.content1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        Infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+  });
